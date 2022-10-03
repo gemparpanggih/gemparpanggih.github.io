98cea028991d1cbfe281983ab260d2a826493a00
Xavier <gemparpanggih123@gmail.com>
2022-10-03 21:24:43 +0800

    push

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-const chk = document.getElementById('chk');
-chk.addEventListener('change', result);
-
 function result() {
     let darkDiv = document.querySelectorAll('.mode');
     for(let x of darkDiv) {
         x.classList.toggle('dark')
     }
+    alert("Mengganti Tema. Klik OK untuk melanjutkan");
 }
